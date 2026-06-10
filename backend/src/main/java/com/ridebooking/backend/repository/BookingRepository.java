0c0a27a20a6cca7b410febebd7b50ece8bc45362
Fathima Aathika <atkilmdn1125@gmail.com>
2026-06-10 20:06:38 +0530

    feat: add BookingRepository.java extending JpaRepository with findByStatus, findByDriverId, findByPassengerPhone query methods

--- backend/src/main/java/com/ridebooking/backend/repository/BookingRepository.java
+++ backend/src/main/java/com/ridebooking/backend/repository/BookingRepository.java
@@ -2,6 +2,7 @@ package com.ridebooking.backend.repository;
 
 import com.ridebooking.backend.model.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
